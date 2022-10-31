3ddb18e85782a49617997f88ee2eec838728d89a
JiriLindovsky <jiri.lindovsky.jr@seznam.cz>
2022-10-31 19:55:53 +0100

    screen

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,12 +4,22 @@ class Counter extends Component {
     state = { 
         count:0,
         imageUrl:'https://picsum.photos/200'
+     }
+     styles1 = {
+        fontSize:10,
+        fontWight:"bold"
      } 
     render() { 
+
+        let classes1= "badge m-2 badge-";
+        classes1+=this.state.count ===0? "warning":"primary";
+
         return (
         <React.Fragment>
             <img src={this.state.imageUrl} alt=""/><br />
-            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span style={ { fontSize:30 }} className={classes1}>{this.formatCount()}</span>
+            {/* <span style={this.styles1} className="badge badge-primary m-2">{this.formatCount()}</span> */}
+
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         )
