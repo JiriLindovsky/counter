7e64e50a4acda6ee3620b9b184bab1af927b5b71
JiriLindovsky <jiri.lindovsky.jr@seznam.cz>
2022-10-31 21:09:57 +0100

    * branch 6

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,7 @@ class Counter extends Component {
 
      handleIncrement = () => {
         console.log("Increment clicked", this);
+        this.setState({count: this.state.count+1});// jinak to nejde, state is "read only"
      }
 
     render() { 
