649ca8442a1daeb6923f5ca512937606c6dae4c1
JiriLindovsky <jiri.lindovsky.jr@seznam.cz>
2022-10-31 19:36:29 +0100

    new version

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    //state = {  } 
+    state = { 
+        count:0,
+        imageUrl:'https://picsum.photos/200'
+     } 
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+        <React.Fragment>
+            <img src={this.state.imageUrl} alt=""/><br />
+            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
+        </React.Fragment>
+        )
+        ;
+    }
+
+    formatCount() {
+        const {count}=this.state;
+        //return count===0 ? <h1>Zero</h1> : count;
+        return count===0 ? 'Zero' : count;
     }
 }
  
