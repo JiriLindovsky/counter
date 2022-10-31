961fe5e3fc6caae661703659655440f3e83b740a
JiriLindovsky <jiri.lindovsky.jr@seznam.cz>
2022-10-31 20:56:41 +0100

    branch 5 handle events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count:0,
-        tags :[]//['tag1','tag2','tag3']
+        count:0
      }
-    renderTags(){
-        if (this.state.tags.length===0) return <p>There are no tags.</p>
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-    }  
+     constructor() {
+        super();
+        this.handleIncrement=this.handleIncrement.bind(this);
+     }
+     handleIncrement(){
+        console.log("Increment clicked", this);
+     }
+
     render() { 
 
-        
         return (
         <React.Fragment>
-               
-            {this.renderTags()}    
-            {this.state.tags.length==0 && "Please create anew tag!"}
-
+            <span className={this.getBadgeClasses1()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         )
         ;
