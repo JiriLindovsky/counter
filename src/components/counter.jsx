07dd0b84ed715daad77c82ddfdcd871ef3a365c9
JiriLindovsky <jiri.lindovsky.jr@seznam.cz>
2022-10-31 20:26:34 +0100

    branch 3 init

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count:0
+        count:0,
+        tags :['tag1','tag2','tag3']
      }
 
     render() { 
@@ -15,6 +16,8 @@ class Counter extends Component {
            
 
             <button className='btn btn-secondary btn-sm'>Increment</button>
+            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+
         </React.Fragment>
         )
         ;
